c360f916910a5221a615a4b1074fcdc1514dc625
Mas Mohamad Rafi Nouval Fadil <rafinouval.f@gmail.com>
2026-07-14 23:10:28 +0700

    feat: set latest version of MLflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
                     "pip": [
                         "pandas",
                         "scikit-learn",
-                        "mlflow==2.19.0",
+                        "mlflow",
                     ]
                 },
             ],
